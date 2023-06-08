7dbef437059c13edefa8d1131a1ef9bc6fb697e0
Ricky <ricky15potter@gmail.com>
2023-06-08 05:54:34 -0500

    Adding questions for the quiz in javascript

---
+++ assets/js/quiz.js
@@ -0,0 +1,54 @@
+//Questions to be used for the quiz
+var questions = [
+    {
+        quizQuestion:"Question #1: In which movie does a princess dance with the villain?",
+        options: ["Moana", "Beauty and the Beast", "Frozen", "Tangled"],
+        correctAnswer: 2,
+    },
+    {
+        quizQuestion:"Question #2: Which two princesses have siblings?",
+        options: ["Merida and Ariel", "Snow White and Belle", "Jasmine and Tiana", "Mulan and Cinderella"],
+        correctAnswer: 0,
+    },
+    {
+        quizQuestion:"Question #3: What is Mickey Mouse's orginal name?",
+        options: ["Tim", "Mortimer", "Monty", "Jasper"],
+        correctAnswer: 1,
+    },
+    {
+        quizQuestion:"Question #4: What is the name of the shark in Finding Nemo?",
+        options: ["Bruce", "Jason", "Daniel", "Jack"],
+        correctAnswer: 0,
+    },
+    {
+        quizQuestion:"Question #5: What is the most expensive movie in disney history?",
+        options: ["Pirates of the Carribbean: At World's End", "Frozen 2", "Tangled", "Brave"],
+        correctAnswer: 0,
+    },
+    {
+        quizQuestion:"Question #6: What was the queens name in sleeping beauty?",
+        options: ["Jasmine", "Rebecca", "Samantha", "She was never given a name"],
+        correctAnswer: 3,
+    },
+    {
+        quizQuestion:"Question #7: Which musical artist/band did lilo copy the most?",
+        options: ["Johnny Cash", "The Beatles", "Jackson 5", "Elvis Presley"],
+        correctAnswer: 4,
+    },
+    {
+        quizQuestion:"Question #8: How many land's are inside the Disneyland park?",
+        options: ["8", "9", "4", "6"],
+        correctAnswer: 1,
+    },
+    {
+        quizQuestion:"Question #9: What is Boo real name from Monster Inc.?",
+        options: ["Sally", "Jessie", "Mary", "Beth"],
+        correctAnswer: 2,
+    },
+    {
+        quizQuestion:"Question #10: How many Disney princesses are there?",
+        options: ["13", "12", "15", "10"],
+        correctAnswer: 0,
+    },
+]
+
